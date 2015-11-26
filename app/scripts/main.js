651ac64f7c6fd6fc5b394acc742bb0d760ccd1f4
Vasyl Posmit <pbgmessenger@gmail.com>
2015-11-26 20:05:24 +0200

    addting app folder

---
+++ app/scripts/main.js
@@ -0,0 +1,13 @@
+console.log('\'Allo \'Allo!');
+
+function toEven(x){
+  var result,
+      evenNumber = {};
+  if (x%2 === 0){result = x}
+  else if (Math.floor(x)%2 === 0) {result = Math.floor(x)+2}
+  else {result = Math.floor(x)+1}
+  evenNumber.old  = x;
+  evenNumber.even = result;
+  evenNumber.gap  = result - x;
+  return evenNumber
+}
